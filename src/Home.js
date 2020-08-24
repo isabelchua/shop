be0ef422eb30c8417e13c130512af95f64c7b423
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-24 12:06:17 -0700

    products added

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Home.css';
+import Product from './Product';
 
 function Home() {
 	return (
@@ -9,6 +10,38 @@ function Home() {
 				className="home__img"
 				alt=""
 			/>
+			<div className="home__row">
+				<Product
+					id="23213"
+					title="Product 1"
+					price={11.99}
+					rating={4}
+					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+				/>
+				<Product
+					id="23213"
+					title="Product 1"
+					price={11.99}
+					rating={4}
+					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+				/>
+				<Product
+					id="23213"
+					title="Product 1"
+					price={11.99}
+					rating={4}
+					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+				/>
+			</div>
+			<div className="home__row">
+				<Product
+					id="23213"
+					title="Product 1"
+					price={11.99}
+					rating={4}
+					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+				/>
+			</div>
 		</div>
 	);
 }
