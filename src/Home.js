bcad8685a299419a3769c884fc675582fec98d8c
isabelchua <60283868+isabelchua@users.noreply.github.com>
2020-08-25 00:59:45 -0700

    added products

--- src/Home.js
+++ src/Home.js
@@ -13,33 +13,33 @@ function Home() {
 			<div className="home__row">
 				<Product
 					id="23213"
-					title="Toy Car"
-					price={11.99}
+					title="Microsoft Surface Headphone with active noise cancellation "
+					price={349.99}
 					rating={4}
-					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+					image="https://images-na.ssl-images-amazon.com/images/I/51z376z5iBL._SL1200_.jpg"
 				/>
 				<Product
 					id="121213"
-					title="Product 2"
-					price={49.99}
-					rating={4}
-					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+					title="RAZER Naga Epic Chroma RZ01-01230100 Black 19 Buttons Tilt Wheel USB Wired / Wireless Laser 8200 dpi MMO Gaming Mouse"
+					price={124.99}
+					rating={3}
+					image="https://c1.neweggimages.com/ProductImage/26-153-180-01.jpg"
 				/>
 				<Product
 					id="3563"
-					title="Product 3"
-					price={99.99}
+					title="Secretlab OMEGA"
+					price={359}
 					rating={4}
-					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+					image="https://cdn.shopify.com/s/files/1/1640/2231/files/turntable_2020_OM_pu_stealth_8-min.jpg"
 				/>
 			</div>
 			<div className="home__row">
 				<Product
 					id="7573"
-					title="Product 4"
-					price={89.99}
-					rating={4}
-					image="https://cdn.shopify.com/s/files/1/0149/8336/4708/products/Fire_Truck_Ladder_Down_Front_View_whitebg_20181017_1024x1024@2x.jpg"
+					title="Ritter Sport Chocolate Bar, Bittersweet Chocolate, 50 Percent Cocoa"
+					price={2.99}
+					rating={5}
+					image="https://images.freshop.com/00050255020001/9c0f2283518f27e7d35a18687778c661_large.png"
 				/>
 			</div>
 		</div>
